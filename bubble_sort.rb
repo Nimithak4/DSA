87421da3d1cff1d240ce817d3891b84f6d9f200f
Nimitha K <nimithak1998@gmail.com>
2026-07-11 12:18:06 +0530

    added bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,14 @@
+def bubble_sort(array)
+  n = array.length
+
+  for i in 0...n-1 do
+    swapped = false
+    for j in 0...n-i-1 do
+      if array[j] > array[j+1]
+        array[j], array[j+1] = array[j+1], array[j]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+end
